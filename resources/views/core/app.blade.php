88d1d7abdf8d653ccbfb83014897252a5c672a70
Adam Albarisyi <adam@baleomol.dev>
2022-12-06 20:29:23 +0700

    WEB-3-tambah filter data member

--- resources/views/core/app.blade.php
+++ resources/views/core/app.blade.php
@@ -1264,6 +1264,19 @@
                 }
             }
         }
+
+
+        const delay = (callback, ms) => {
+            var timer = 0;
+            return function() {
+                var context = this,
+                    args = arguments;
+                clearTimeout(timer);
+                timer = setTimeout(function() {
+                    callback.apply(context, args);
+                }, ms || 0);
+            };
+        }
     </script>
 
     @stack('js')
