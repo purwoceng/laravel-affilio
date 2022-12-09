cdf1fd18157498bc3b72fe8d16fff2428c210dc6
purwoceng <rahmadipurwo@gmail.com>
2022-12-09 08:56:50 +0700

    fix some

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Repositories\Interfaces\MemberRepositoryInterface;
 use App\Repositories\MemberRepository;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -34,5 +35,6 @@ class AppServiceProvider extends ServiceProvider
     {
         config(['app.locale' => 'id']);
 	    Carbon::setLocale('id');
+        Schema::defaultStringLength(191);
     }
 }
