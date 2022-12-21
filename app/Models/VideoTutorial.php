b50b7d4e2c8afa8e39658d94b49d6ea626566579
Dandi Pratama <dandi@aplikasix.dev>
2022-12-21 20:54:14 +0700

    Add video tutorial models

---
+++ app/Models/VideoTutorial.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VideoTutorial extends Model
+{
+    use HasFactory;
+}
