e655342b9d5426c0024809fc6c768c109ae702ac
Adam Albarisyi <adam@baleomol.dev>
2022-12-21 20:09:13 +0700

    add migration orders

--- database/migrations/2022_12_20_212434_create_order_products_table.php
+++ database/migrations/2022_12_20_212434_create_order_products_table.php
@@ -17,14 +17,20 @@ class CreateOrderProductsTable extends Migration
             $table->id();
             $table->foreignId('order_id');
             $table->foreignId('origin_product_id');
-            $table->string('product_name', 191);
-            $table->integer('price_markup');
+            $table->string('product_name', 256);
             $table->integer('quantity');
+            $table->integer('weight');
+            $table->integer('total_weight');
+            $table->integer('price_markup');
+            $table->integer('total_price_markup');
             $table->integer('origin_price');
             $table->integer('origin_total_price');
             $table->integer('price');
             $table->integer('total_price');
-            $table->json('variant')->nullable();
+            $table->integer('profit')->default('0');
+            $table->integer('total_profit')->default('0');
+            $table->foreignId('variant_id')->nullable();
+            $table->string('variant_name',128)->nullable();
             $table->timestamps();
         });
     }
