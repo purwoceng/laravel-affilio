8a0b06c5a6480db0e09330d88fe557155eb915fe
Dandi Pratama <dandi@aplikasix.dev>
2022-12-22 15:29:34 +0700

    Add models and seeder for video tutorial

--- app/Http/Controllers/VideoTutorial/VideoTutorialController.php
+++ app/Http/Controllers/VideoTutorial/VideoTutorialController.php
@@ -9,16 +9,85 @@ use Illuminate\Http\Request;
 class VideoTutorialController extends Controller
 {
     protected $repository;
+    public $member_types;
 
     public function __construct(VideoTutorialRepository $video_tutorial_repository)
     {
         $this->repository = $video_tutorial_repository;
+        $this->member_types = [
+            [
+                'id' => 1,
+                'name' => 'Member',
+            ],
+            [
+                'id' => 2,
+                'name' => 'Core member',
+            ],
+            [
+                'id' => 3,
+                'name' => 'Leader',
+            ],
+            
+            [
+                'id' => 4,
+                'name' => 'Super Leader',
+            ],
+        
+            [
+                'id' => 5,
+                'name' => 'Bronze',
+            ],
+    
+            [
+                'id' => 6,
+                'name' => 'Super Bronze',
+            ],
+
+            [
+                'id' => 7,
+                'name' => 'Silver',
+            ],
+
+            [
+                'id' => 8,
+                'name' => 'Super Silver',
+            ],
+
+            [
+                'id' => 9,
+                'name' => 'Gold',
+            ],
+
+            [
+                'id' => 10,
+                'name' => 'Super Gold',
+            ],
+            [
+                'id' => 11,
+                'name' => 'Platinum',
+            ],
+            [
+                'id' => 12,
+                'name' => 'Super Platinum',
+            ],
+            [
+                'id' => 13,
+                'name' => 'Diamond',
+            ],
+            [
+                'id' => 14,
+                'name' => 'Super Diamond',
+            ],
+        ];
     }
 
     public function index(Request $request)
     {
         if ($request->ajax()) {
             $result = $this->repository->getDataTable($request);
+            $data = $result['data'];
+
+            
 
             return response()->json($result);
         }
--- app/Models/VideoTutorial.php
+++ app/Models/VideoTutorial.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class VideoTutorial extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'video_tutorials';
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
+        'member_type_id',
+        'name',
+        'url',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+        'deleted_at' => 'datetime:Y-m-d H:i:s',
+    ];
 }
