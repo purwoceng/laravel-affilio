7643a0f8e9c6eed46808c976a10f4f87ed3d60ed
Adam Albarisyi <adam@baleomol.dev>
2023-01-24 22:03:33 +0700

    add dummy data

--- database/seeders/MemberTableSeeder.php
+++ database/seeders/MemberTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Member;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class MemberTableSeeder extends Seeder
 {
@@ -14,6 +15,23 @@ class MemberTableSeeder extends Seeder
      */
     public function run()
     {
-        Member::factory()->count(50)->create();
+        // Member::factory()->count(50)->create();
+
+        $members = json_decode(file_get_contents(public_path() . "/dummy_data/member.json"), true);
+
+        foreach ($members as $key => $value) {
+            Member::create([
+                "member_type_id" => $value['member_type_id'],
+                "username" => $value['username'],
+                "email" => $value['email'],
+                "name" => $value['name'],
+                "phone" => $value['phone'],
+                "hash" => Hash::make($value['hash']),
+                "referral" => $value['referral'],
+                "is_verified" => $value['is_verified'],
+                "is_blocked" => $value['is_blocked'],
+                "publish" => $value['publish'],
+            ]);
+        }
     }
 }
--- database/seeders/OrderProductSeeder.php
+++ database/seeders/OrderProductSeeder.php
@@ -14,6 +14,29 @@ class OrderProductSeeder extends Seeder
      */
     public function run()
     {
-        OrderProduct::factory(50)->create();
+        // OrderProduct::factory(50)->create();
+
+        $orderProducts = json_decode(file_get_contents(public_path() . "/dummy_data/order_products.json"), true);
+
+        foreach ($orderProducts as $key => $value) {
+            OrderProduct::create([
+                "order_id" => $value['order_id'],
+                "origin_product_id"=> $value['origin_product_id'],
+                "product_name"=> $value['product_name'],
+                "quantity"=> $value['quantity'],
+                "weight"=> $value['weight'],
+                "total_weight"=> $value['total_weight'],
+                "price_markup"=> $value['price_markup'],
+                "total_price_markup"=> $value['total_price_markup'],
+                "origin_price"=> $value['origin_price'],
+                "total_origin_price"=> $value['total_origin_price'],
+                "price"=> $value['price'],
+                "total_price"=> $value['total_price'],
+                "profit"=> $value['profit'],
+                "total_profit"=> $value['total_profit'],
+                "variant_id"=> $value['variant_id'],
+                "variant_name"=> $value['variant_name'],
+            ]);
+        }
     }
 }
