31aee8c4552f5259fc7ce6cc5830298b62f4828a
Purwo <purwo@aplikasix.dev>
2023-05-15 09:37:52 +0700

    members

--- app/Http/Controllers/Member/MemberController.php
+++ app/Http/Controllers/Member/MemberController.php
@@ -33,8 +33,12 @@ class MemberController extends Controller
         //nganggo iki yo iso
         // $getMemberBlockeds = Member::with('member_addresses')->where('id')->where('publish', 1)->get();
 
-        $getMemberBlockeds = DB::table('members')->where('members.publish','1')
-                            ->join('member_addresses','members.id', '=', 'member_addresses.member_id')
+        // $getMemberBlockeds = DB::table('members')->where('members.publish','1')
+        //                     ->join('member_addresses','members.id', '=', 'member_addresses.member_id')
+        //                     ->get();
+         $getMemberBlockeds = DB::table('members')
+                            ->leftjoin('member_addresses','members.id','=','member_addresses.member_id')
+                            ->where('members.publish','1')
                             ->get();
 
         $member_type = MemberType::get();
